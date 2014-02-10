50aa2e5936e733ca2c90157a860d29108de8dc11
zhuoxiaolong <zhuoxiaolong@gmail.com>
2014-02-10 17:05:28 +0800

    modified:   config/deploy/staging.rb

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,10 +1,10 @@
 set :user, "ubuntu"
 set :password, "Gv0NDlO2Cw29N9"
-set :deploy_to, "/var/www/apps/weixin_game"
+set :deploy_to, "/data/www/apps/weixin_game"
 
-role :app, "10.221.84.136"
-role :web, "10.221.84.136"
-role :db,  "10.221.84.136", primary: true
+role :app, "203.195.186.54"
+role :web, "203.195.186.54"
+role :db,  "203.195.186.54", primary: true
 # tasks
 namespace :deploy do
   task :start, :roles => :app do
@@ -21,8 +21,7 @@ namespace :deploy do
 
   desc "Restart Application"
   task :restart, :roles => :app do
-    run "mv #{current_path}/config/database.yml.example #{current_path}/config/database.yml"
-    #run "mv #{current_path}/config/database.yml.example #{current_path}/config/database.yml"
+    #run "mv #{current_path}/config/database.yml.example1 #{current_path}/config/database.yml"
     #run "cd #{current_path} && RAILS_ENV=production bundle exec rake assets:precompile"
     #run "touch #{current_path}/tmp/restart.txt"
   end
