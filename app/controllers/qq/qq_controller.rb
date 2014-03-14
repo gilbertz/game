c662d36cdd4dd6304e643629b9971d899f383aeb
zl <themorecolor@gmail.com>
2014-03-14 15:20:48 +0800

    update

--- app/controllers/qq/qq_controller.rb
+++ app/controllers/qq/qq_controller.rb
@@ -5,9 +5,18 @@ class Qq::QqController < ActionController::Base
   before_action :set_header_for_qq
   layout "qq/layouts/qq_game"
 
+
+  before_action :get_platform_info
+
   private
   def set_header_for_qq
     response.headers["X-Frame-Options"] = "Allow-From http://rc.qzone.qq.com"
   end
+
+  def get_platform_info
+    @pf = params[:pf] || "qzone"
+    puts @pf
+  end
+
 
 end
