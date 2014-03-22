7ac2424df40e1c827acc459b935f1eaad8b8ae16
zl <themorecolor@gmail.com>
2014-03-22 11:44:50 +0800

    update

---
+++ config/deploy/development.rb
@@ -0,0 +1,28 @@
+set :user, "ubuntu"
+set :password, "JScY5w2K3Kd"
+set :deploy_to, "/data/long/apps/game_dev"
+
+role :app, "203.195.191.203"
+role :web, "203.195.191.203"
+role :db,  "203.195.191.203", primary: true
+# tasks
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  task :trust_rvmrc do
+    #run "rvm rvmrc trust #{latest_release}"
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "mv #{current_path}/config/database.yml.example #{current_path}/config/database.yml"
+    run "cd #{current_path} && RAILS_ENV=production bundle exec rake assets:precompile"
+    run "cd #{current_path} && passenger stop -p 5002 && passenger start --daemonize -p 5002"
+  end
+end
