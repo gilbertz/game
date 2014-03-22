d4bfe294443fb748f0c5b1999657b3d84e7e026b
zl <themorecolor@gmail.com>
2014-03-22 11:28:40 +0800

    update for qq test

--- app/controllers/qq/welcome_controller.rb
+++ app/controllers/qq/welcome_controller.rb
@@ -1,7 +1,51 @@
 class Qq::WelcomeController < Qq::QqController
 
+  Api_domain = "openapi.tencentyun.com"
+  Api_url = "http://#{Api_domain}/"
+
+
+  App_id = "1101255082"
+  App_key = "rwgnIjHffqSXS4D3"
+
   def index
+    if params[:index].to_i == 10
+      render :template => "qq/welcome/index", :layout => false
+      return
+    end
+
+    p = pre_format_p
+    sig = sign(p, App_key)
+
+    puts sig
+    puts "============="
+
+    p.merge!(sig: sig)
+    @res = RestClient.get(Api_url + "v3/user/get_info", p)
+
     render :template => "qq/welcome/gabrielecirulli", :layout => false
   end
+
+  private
+  def pre_format_p
+    {
+        openid: params[:openid] || "",
+        openkey: params[:openkey] || "",
+        pf: params[:pf] || "",
+        appid: App_id
+    }
+  end
+
+
+  def sign(p, secret)
+    method = "/v3/user/get_info"
+    str1 = "#{CGI::escape(method)}"
+    str2 = CGI::escape(p.sort.collect{|i| i.join("=")}.join("&"))
+    str = "GET&#{str1}&#{str2}"
+
+    secret = "#{secret}&"
+    digest = OpenSSL::Digest::Digest.new('sha1')
+    Base64.encode64(OpenSSL::HMAC.digest(digest, secret, str)).chomp
+  end
+
 
 end
