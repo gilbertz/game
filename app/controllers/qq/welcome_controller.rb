5af1fc600ace13be28f052212179d04162be1850
zl <themorecolor@gmail.com>
2014-03-27 10:21:54 +0800

    update

--- app/controllers/qq/welcome_controller.rb
+++ app/controllers/qq/welcome_controller.rb
@@ -8,7 +8,7 @@ class Qq::WelcomeController < Qq::QqController
   App_key = "rwgnIjHffqSXS4D3"
 
   def index
-    @material_id = params[:material_id] || 41
+    @material_id = params[:material_id] || 311
     @material = Material.find @material_id
 
     @from = "qq"
