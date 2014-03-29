fc17ef7ce08c5e165885d574115fcc8522c64577
zl <themorecolor@gmail.com>
2014-03-29 14:59:16 +0800

    update application exception.log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from StandardError do |exception|
+    new_logger = Logger.new('log/exceptions.log')
+    new_logger.info('THIS IS A NEW EXCEPTION!')
+    new_logger.info(exception.message)
+    new_logger.info(exception.backtrace)
+    raise exception
+  end
+
+
 end
