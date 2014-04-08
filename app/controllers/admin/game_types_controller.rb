61361daf6776bf243b11e32c25ed09c569d674a7
zl <themorecolor@gmail.com>
2014-04-08 19:47:43 +0800

    update

--- app/controllers/admin/game_types_controller.rb
+++ app/controllers/admin/game_types_controller.rb
@@ -14,6 +14,17 @@ class Admin::GameTypesController < Admin::BaseController
     redirect_to :back
   end
 
+  def edit
+    @game_type = GameType.find(params[:id])
+  end
+
+  def update
+    game_type = GameType.find(params[:id])
+    game_type.update_attributes(game_type_params)
+    game_type.save
+    redirect_to admin_game_types_path
+  end
+
   private
   def game_type_params
     params.require(:game_type).permit(:game_type)
