07fed2189afd7c6819dbda0968f0e4152addcb51
wps <new@sjb.com>
2014-04-09 22:06:56 +0800

    cao

--- app/models/material.rb
+++ app/models/material.rb
@@ -39,10 +39,6 @@ class Material < ActiveRecord::Base
   def cn_state; { 0 => '下线', 1 => '上线', nil => '下线' }[state] end
 
   def game_type
-
-    puts self.category.inspect
-    puts "============="
-
     unless self.category.game_type.blank?
       self.category.game_type.game_type
     else
