d1c0eef146a10c14d1fd240bf2a446c37083dfbc
zl <themorecolor@gmail.com>
2014-04-26 18:38:03 +0800

    update for qq

--- app/controllers/qq/qq_controller.rb
+++ app/controllers/qq/qq_controller.rb
@@ -10,6 +10,20 @@ class Qq::QqController < ActionController::Base
   before_action :get_platform_info
   before_action :get_user_info
 
+
+  rescue_from StandardError do |exception|
+    new_logger = Logger.new('log/qq_exceptions.log')
+    new_logger.info("THIS IS A NEW EXCEPTION! #{Time.now.to_s}")
+    new_logger.info("------ #{request.remote_ip} ------")
+    new_logger.info(request.url)
+    new_logger.info(params.to_s)
+    new_logger.info(exception.message)
+    new_logger.info(exception.backtrace.join("\n"))
+    raise exception
+  end
+
+
+
   CONFIG = {
       qzone: {
           'appid' => '1101255082',
