3364dd9c3891f13dcd76242e74f41ddb28138c22
root <root@AY140521225939574bc7Z.(none)>
2014-09-21 08:57:22 +0800

    change share jump link

--- app/controllers/weitest_controller.rb
+++ app/controllers/weitest_controller.rb
@@ -148,7 +148,8 @@ class WeitestController < ApplicationController
     @material = Material.find_by_url params[:id]
     #@material.wx_ln = 'http://mp.weixin.qq.com/s?__biz=MzA4NTkwNTIxOQ==&mid=201004496&idx=1&sn=c3dcb0820a5c3746991de7de63429fc8#rd'
     #@material.wx_ln = "http://mp.weixin.qq.com/s?__biz=MzA3MDk0MzMxNQ==&mid=200586729&idx=1&sn=599156aecedbfa9317785390ddb0b448#rd"
-    @material.wx_ln = "http://mp.weixin.qq.com/s?__biz=MjM5NjIzOTE2OQ==&mid=200366500&idx=1&sn=21c2832b5382e6aafd4e55a0d6c85f5f#rd"
+    #@material.wx_ln = "http://mp.weixin.qq.com/s?__biz=MjM5NjIzOTE2OQ==&mid=200366500&idx=1&sn=21c2832b5382e6aafd4e55a0d6c85f5f#rd"
+    @material.wx_ln = "http://share.51self.com"
 
     @base_category = Category.find(1)
     get_topn(@material.category_id)
