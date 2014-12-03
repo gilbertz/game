5782fa0c6054ad8d9017cb992eeed28b2b40c1f3
root <root@AY140521225939574bc7Z.(none)>
2014-12-03 11:18:33 +0800

    abc

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -7,7 +7,7 @@ class Domain < ActiveRecord::Base
   def get_name
     tags = ['weixin', 'ifeng', 'jd', 'apple', 'sogou']
     tag = tags.sample(1)[0]
-    self.name.gsub("*", "t"+rand(10000).to_s )
+    self.name.gsub("*", rand(1000).to_s )
     #self.name.gsub("*", "mp.weixin.qq.com")
   end
 end
