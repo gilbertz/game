28dcdd1b6f3453c2b4669d362959de9a17577f5b
root <root@iZ28bm44n2pZ.(none)>
2015-04-16 17:11:57 +0800

    share show menu

--- app/controllers/wshows_controller.rb
+++ app/controllers/wshows_controller.rb
@@ -1,9 +1,6 @@
 class WshowsController < ApplicationController
   def show
     @wshow = Wshow.find params[:id]
-    # puts Beaconid.find_by_beaconid(params[:beaconid])
-    id = Ibeacon.find_by(:url=>params[:beaconid]).id
-    @rp = Redpack.find_by(beaconid: id).weixin_post.to_i/100
     render layout: false
   end
 
