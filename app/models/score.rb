e88ad1a040ac573bc5f5d4900dcb45e3011e4d13
gilbertz <zhaoguoqi@sjtu.edu.cn>
2015-04-20 18:26:15 +0800

    cc

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,2 +1,26 @@
 class Score < ActiveRecord::Base
+  belongs_to :user
+  
+  
+  def beacon_name
+    if self.beaconid
+      b = Ibeacon.find self.beaconid
+      return b.name if b
+    end
+  end
+
+  def user_name
+    self.user.name
+  end
+
+  def app_name
+    if self.game_id
+      m = Material.find_by_id self.game_id
+      if m
+        return m.name
+      end
+    end
+  end
+
+
 end
