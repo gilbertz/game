5b0d0604c6d45a13d12515d7cf76ebcc21092ac5
ct <ctaodream@163.com>
2015-05-05 01:20:26 +0800

    get user info

--- app/controllers/wx_app_auth_controller.rb
+++ app/controllers/wx_app_auth_controller.rb
@@ -7,8 +7,10 @@ class WxAppAuthController < ApplicationController
     appid = params["appid"]
     if appid
       redirect_to authurl(appid,"snsapi_userinfo")
+      return
     end
 
+    render :text => ""
   end
 
   def callback
@@ -20,8 +22,35 @@ class WxAppAuthController < ApplicationController
     if code == nil && state != STATE
       return
     else
+      appid = params["appid"]
+      app_auth_info = get_app_auth_info(appid,code)
+
+      openid = app_auth_info["openid"]
+      scope = app_auth_info["scope"]
+      # 非静默授权 查询用户的信息保存下来
+      if scope == "snsapi_userinfo"
+        access_token = app_auth_info["access_token"]
+        Thread.new do
+          user_info = get_user_info(openid,access_token)
+          p "user_info = #{user_info}"
+        end
+       # 静默授权
+      elsif scope == "snsapi_base"
+
+
+      end
+
+      redirect_to dispatch_url(appid,openid)
 
     end
+
+    render :text => ""
+  end
+
+
+
+  def dispatch_url(appid,openid)
+    "http://www.baidu.com"
 
   end
 
