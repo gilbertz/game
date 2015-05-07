5df4b87dc8017d9b46a7d978c43c28f79fb3e09f
ct <ctaodream@163.com>
2015-05-07 21:57:52 +0800

    qr

--- app/controllers/qrcode_controller.rb
+++ app/controllers/qrcode_controller.rb
@@ -1,12 +1,14 @@
+require File.expand_path('../qrcode_scene_type',__FILE__)
 class QrcodeController < ApplicationController
 
+  include QrcodeSceneType
   def query_scaner
     @scaner = nil
     ticket = params["ticket"]
     if ticket
       qrcode = Qrcode.find_by_ticket(ticket)
 
-      if qrcode && qrcode.expire_at.to_i < Time.now.to_i
+      if qrcode && qrcode.expire_at.to_i < Time.now.to_i && qrcode.scene_type == LOGIN_SCENE
         @scaner = qrcode.scaner
       end
     end
