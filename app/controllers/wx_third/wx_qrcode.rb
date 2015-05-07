6919d50af604ad3e4b5ae99e830e51b0c7b8f3bf
ct <ctaodream@163.com>
2015-05-07 19:47:28 +0800

    scan qrcode modift

--- app/controllers/wx_third/wx_qrcode.rb
+++ app/controllers/wx_third/wx_qrcode.rb
@@ -1,10 +1,10 @@
 module WxQrcode
 
-  def generate_qr(access_token,is_tempoart = true)
+  def generate_qr(appid,access_token,is_tempoart = true)
     if access_token.blank?
       return nil
     end
-    url = "https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=#{access_token}"
+    url = "https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=#{access_token}"
     post_data = nil
     if is_tempoart
       post_data = {"expire_seconds"=> 604800, "action_name"=> "QR_SCENE", "action_info"=> {"scene"=> {"scene_id"=> 123}}}
