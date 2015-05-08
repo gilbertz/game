6f414c6f9ac3f40502930f4713eb9d0235ae5304
ct <ctaodream@163.com>
2015-05-08 17:38:10 +0800

    templte msg

--- app/controllers/qrcode_controller.rb
+++ app/controllers/qrcode_controller.rb
@@ -25,11 +25,28 @@ class QrcodeController < ApplicationController
     $wxclient.send_text_custom("oRKD0s8stWW-DUiWIKDKV22qaUVI","1245wwwwww")
 
     $wxclient1.send_text_custom("oNnqbt_LiqkMXMrzHEawO-G9r8Vo","1245wwwwww")
-    $wxclient1.send_template_msg("oNnqbt_LiqkMXMrzHEawO-G9r8Vo", "1-DZpzUOCJ-Es-QLgSS0mu83fZ-O9w6iWm0hZKSq8G8", "http://www.dapeimishu.com/", "#FF0000",  "data"=>{
+    $wxclient1.send_template_msg("oNnqbt_LiqkMXMrzHEawO-G9r8Vo", "hMQm4-BGvNX-XIRQnfb_MG3EP6AFCDEFJ0gPrBX7oeg", "http://www.dapeimishu.com/", "#FF0000",  "data"=>{
         "first"=> {
             "value"=>"恭喜你购买成功！",
             "color"=>"#173177"
-        }})
+        },
+        "keynote1"=>{
+        "value"=>"巧克力",
+        "color"=>"#173177"
+    },
+        "keynote2"=>{
+        "value"=>"39.8元",
+        "color"=>"#173177"
+    },
+        "keynote3"=> {
+        "value"=>"2014年9月16日",
+        "color"=>"#173177"
+    },
+        "remark"=>{
+        "value"=>"欢迎再次购买！",
+        "color"=>"#173177"
+    }
+    })
 
     render :json => result.to_json
   end
