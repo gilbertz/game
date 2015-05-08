58d549a9ffd7608928494e311c8cc83c30019c6a
gilbertz <zhaoguoqi@sjtu.edu.cn>
2015-05-08 20:20:51 +0800

    aa

--- restart.sh
+++
@@ -1,5 +0,0 @@
-#/etc/init.d/redis-server restart
-#RAILS_ENV=production bundle exec rake assets:precompile
-#rm log/*log -rf
-passenger stop --port 7000
-passenger start --daemonize --port 7000 -e production
