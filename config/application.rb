ebf6e7143a38ce9c75c5981f6d3e71ea2f14fa2f
gilbertz <zhaoguoqi@sjtu.edu.cn>
2015-05-11 23:16:28 +0800

    ok

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.eager_load_paths += ["#{Rails.root}/lib"]
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
