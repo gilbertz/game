d1c7ff96eee8a81156bf8c0b32516a98a1b10abc
ct <ctaodream@163.com>
2015-05-14 16:46:35 +0800

    sidekiq modify

--- app/controllers/wx_third_auth_controller.rb
+++ app/controllers/wx_third_auth_controller.rb
@@ -121,9 +121,9 @@ class WxThirdAuthController < ApplicationController
             Thread.new {  deal_card(authorizer_appid)
  			  p "this is new thread!"
             $redis.set("h",1)
-           WxUtil.save_users(authorizer_appid)
+           # WxUtil.save_users(authorizer_appid)
             }
-            #AuthenticationUserWork.perform_async(authorizer_appid,SHAKE_APPID)
+            AuthenticationUserWork.perform_async(authorizer_appid,SHAKE_APPID)
         end
         render :json => {"result"=> "success"}.to_json
       end
--- app/workers/authentication_user_work.rb
+++ app/workers/authentication_user_work.rb
@@ -4,11 +4,9 @@ class AuthenticationUserWork
   include Sidekiq::Worker
 
   def perform(authentication_appid,component_appid)
-    $redis.set("f",40)
+    $redis.set("h",4)
     p "xxxxx========"
-
     WxUtil.save_users(authentication_appid)
-
   end
 
 end
