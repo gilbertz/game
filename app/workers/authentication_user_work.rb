fed03d6ae59cc40b00485539faa2728f7dcf096c
ct <ctaodream@163.com>
2015-05-15 15:41:21 +0800

    sidekiq config first

--- app/workers/authentication_user_work.rb
+++ app/workers/authentication_user_work.rb
@@ -2,7 +2,7 @@ require File.expand_path('../../controllers/wx_third/wx_util',__FILE__)
 class AuthenticationUserWork
 
   include Sidekiq::Worker
-  sidekiq_options :retry => 5
+  sidekiq_options :retry => 5,:backtrace => true
 
   sidekiq_retries_exhausted do |msg|
     Sidekiq.logger.warn "Failed #{msg['class']} with #{msg['args']}: #{msg['error_message']}"
--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module Game
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Beijing'
 
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
