dd6a4bd87e2afcdeb5d2eab2a2286af17448c174
ct <ctaodream@163.com>
2015-05-18 02:22:18 +0800

    grape api

---
+++ app/api/cards/card_api.rb
@@ -0,0 +1,39 @@
+module API
+
+  module Cards
+
+    module Entities
+      class CardOption < Grape::Entity
+        expose :title
+        expose :img
+        expose :store
+      end
+
+      class Card < Grape::Entity
+        expose :title
+        expose :card_options, using: API::CardOption
+      end
+    end
+
+    class CardAPI < Grape::API
+      version      'v1'
+
+      namespace :cards do
+
+        desc "Return a card."
+        params do
+          requires :id, type: Integer, desc: "card id."
+        end
+        route_param :id do
+          get do
+            Card.find(params[:id])
+          end
+        end
+
+      end
+
+    end
+
+  end
+
+end
