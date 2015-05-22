97c07a94fda47c0dd819b6b31a950711b5a8706e
ct <ctaodream@163.com>
2015-05-22 20:53:40 +0800

    weixin_authorize config redis

--- config/initializers/weixin_authorize.rb
+++ config/initializers/weixin_authorize.rb
@@ -1,7 +1,9 @@
+require 'redis'
 require 'redis-namespace'
 # 这里修改成你的的命名空间。
 namespace = "ibeacon:weixin_authorize"
-redis = nil
+
+redis  = nil
 if Rails.env.production?
   REDIS_HOST = '211ba332fe0a11e4.m.cnqda.kvstore.aliyuncs.com'
   redis =  Redis.new(:host => REDIS_HOST,:password => '211ba332fe0a11e4:Dapei123', :port => 6379)
@@ -16,8 +18,8 @@ exist_keys.each{|key|redis.del(key)}
 
 # Give a special namespace as prefix for Redis key, when your have more than one project used weixin_authorize, this config will make them work fine.
 # => #<Redis client v3.1.0 for redis://127.0.0.1:6379/0>
-namespaced_redis = Redis::Namespace.new("#{namespace}", :redis => redis)
+# namespaced_redis = Redis::Namespace.new("#{namespace}", :redis => redis)
 
 WeixinAuthorize.configure do |config|
-  config.redis = namespaced_redis
+  config.redis = redis
 end
