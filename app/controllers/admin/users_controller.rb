2bc344f230647c2cf5f861733d20e1448f1778f8
gilbertz <zhaoguoqi@sjtu.edu.cn>
2015-05-25 11:48:41 +0800

    hi, records

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,11 @@
 class Admin::UsersController < Admin::BaseController
 
   def index
-    @users = User.order('id desc')
+    limit = 20
+    limit = params[:limit].to_i if params[:limit]
+    cond = '1=1'
+    cond += " and name like '%#{params[:q]}%'" if params[:q]
+    @users = User.where(cond).order('created_at desc').page(params[:page]).per(limit)
   end
 
   def new
