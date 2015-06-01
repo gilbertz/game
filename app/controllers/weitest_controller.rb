3b496363c7c9787ebc6ea1d232bd476d531d1637
gilbertz <gilbertzhaoguoqi@gmail.com>
2015-06-01 23:07:20 +0800

    redpack

--- app/controllers/weitest_controller.rb
+++ app/controllers/weitest_controller.rb
@@ -47,7 +47,7 @@ class WeitestController < ApplicationController
   end
 
   def seed_redpack
-    # if user_agent? 
+    if user_agent? 
     if request.headers['secret'] == "yaoshengyi"
       @rp = 0
       redpack_time = RedpackTime.get_redpack_time(@object.id)
@@ -57,10 +57,6 @@ class WeitestController < ApplicationController
         check = Check.find_by(user_id: current_user.id, beaconid: beaconid,state: 1,game_id: params[:game_id])
         check.update(:state => 0) if check
         info = Redpack.gain_seed_redpack(current_user.id, params[:game_id], @object, @beacon.id)
-        p current_user.id 
-        p  @object.id
-        p @beacon.id
-        p info
         @rp = Redpack.find(@object.id).weixin_post(current_user,params[:beaconid],info) if info >100
         render :status => 200, json: {'info' => @rp.to_i}
       else # Record.redpack_per_day(current_user.id, params[:game_id]) == 3
@@ -70,7 +66,7 @@ class WeitestController < ApplicationController
     else
       render :status => 200, json: {'info' => request.headers['secret']}
     end
-    # end
+    end
    # render :status => 200, json: {"info" => "六一儿童节快乐", "name" => current_user.id}
   end
 
