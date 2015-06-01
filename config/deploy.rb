7d3b5c477a30afe2534946fcf249b378c16fc2a3
ct <ctaodream@163.com>
2015-06-01 21:15:40 +0800

    deploy modify

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,13 +9,21 @@ require 'mina/rvm'    # for rvm support. (http://rvm.io)
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
+ENV["to"] ||= '121'
+case ENV["to"]
+  when '121'
+    set :domain, '121.42.47.121'
+    set :user, 'root'
+    set :password, 'bNg42mjv'
+    set :deploy_to, '/data/www/mina'
+    ENV['port'] = '2016'
 
-set :domain, '121.42.47.121'
-set :deploy_to, '/data/www/mina'
+  when '114'
+    
+end
+
 set :repository, 'https://leapcliff:831022@git.coding.net/leapcliff/ibeacon.git'
 set :branch, 'master'
-
-ENV['port'] = '2016'
 
 # For system-wide RVM install.
 set :rvm_path, '/usr/local/rvm/bin/rvm'
@@ -23,9 +31,10 @@ set :rvm_path, '/usr/local/rvm/bin/rvm'
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, ['config/database.yml', 'log']
+set :keep_releases, 5
 
 # Optional settings:
-   set :user, 'root'    # Username in the server to SSH to.
+ #  set :user, 'root'    # Username in the server to SSH to.
  #  set :passwd, '111111'
 #   set :port, '30000'     # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
@@ -64,13 +73,13 @@ task :passenger => :environment do
 end
 
 task :passenger_start => :environment do
-#  queue "source /etc/profile.d/rvm.sh" 
+  queue "source /etc/profile.d/rvm.sh"
  # queue "cd #{deploy_to}/#{current_path} && rvmsudo /usr/local/rvm/gems/ruby-2.0.0-p481/bin/passenger start -a 0.0.0.0 -p #{ENV['port']} -d -e production --pid-file #{deploy_to}/#{shared_path}/passenger.#{ENV['port']}.pid"
   queue "cd #{deploy_to}/#{current_path} && rvmsudo /usr/local/rvm/gems/ruby-2.0.0-p481/bin/passenger  start -a 0.0.0.0 -p #{ENV['port']} -d -e production --pid-file #{deploy_to}/#{shared_path}/passenger.#{ENV['port']}.pid"
 end
 
 task :passenger_stop => :environment do
-  #queue "source /etc/profile.d/rvm.sh" 
+  queue "source /etc/profile.d/rvm.sh"
   #quene "touch #{deploy_to}/#{current_path}/passenger.#{ENV['port']}.pid"
   #queue "cd #{deploy_to}/#{current_path} && rvmsudo /usr/local/rvm/gems/ruby-2.0.0-p481/bin/passenger stop -p #{ENV['port']} --pid-file #{deploy_to}/#{shared_path}/passenger.#{ENV['port']}.pid"
   quene "touch #{deploy_to}/#{current_path}/passenger.#{ENV['port']}.pid"
@@ -104,6 +113,14 @@ task :deploy => :environment do
   end
 end
 
+
+
+desc "Rollback to previous verison."
+task :rollback => :environment do
+  queue %[echo "----> Start to rollback"]
+  queue %[if [ $(ls #{deploy_to}/releases | wc -l) -gt 1 ]; then echo "---->Relink to previos release" && unlink #{deploy_to}/current && ln -s #{deploy_to}/releases/"$(ls #{deploy_to}/releases | tail -2 | head -1)" #{deploy_to}/current && echo "Remove old releases" && rm -rf #{deploy_to}/releases/"$(ls #{deploy_to}/releases | tail -1)" && echo "$(ls #{deploy_to}/releases | tail -1)" > #{deploy_to}/last_version && echo "Done. Rollback to v$(cat #{deploy_to}/last_version)" ; else echo "No more release to rollback" ; fi]
+  invoke :passenger
+end
 # For help in making your deploy script, see the Mina documentation:
 #
 #  - http://nadarei.co/mina
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
-bundle check
-cap production deploy
-cap staging deploy
+#bundle check
+#cap production deploy
+#cap staging deploy
+
+mina setup
+mina deploy to=121
