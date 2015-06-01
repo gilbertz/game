db7f0ea66b9c6ae70809fe8e5478c30093333d14
ct <ctaodream@163.com>
2015-06-01 22:14:49 +0800

    deploy 114

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ require 'mina/rvm'    # for rvm support. (http://rvm.io)
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
-ENV["to"] ||= '121'
+ENV["to"] ||= '114'
 case ENV["to"]
   when '121'
     set :domain, '121.42.47.121'
@@ -17,9 +17,12 @@ case ENV["to"]
     set :password, 'bNg42mjv'
     set :deploy_to, '/data/www/mina'
     ENV['port'] = '2016'
-
   when '114'
-    
+    set :domain, '114.215.120.243'
+    set :user, 'root'
+    set :password, '449e1fb5'
+    set :deploy_to, '/data/www/mina'
+    ENV['port'] = '2016'
 end
 
 set :repository, 'https://leapcliff:831022@git.coding.net/leapcliff/ibeacon.git'
