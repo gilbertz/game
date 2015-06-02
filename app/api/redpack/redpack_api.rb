423a308081f9402d52a66d83f28283141bac8736
gilbertz <gilbertzhaoguoqi@gmail.com>
2015-06-02 17:33:03 +0800

    red

--- app/api/redpack/redpack_api.rb
+++ app/api/redpack/redpack_api.rb
@@ -141,12 +141,7 @@ module API
           if check_num <= person_num and check_state
             check_state.update(:state => 0) 
             info = Redpack.gain_seed_redpack(current_user.id, game_id, object, beacon_id)
-            p current_user.id
-            p game_id
-            p object.id
-            p beacon_id
-            p info
-            money = Redpack.find(object.id).weixin_post(current_user,beacon_id,info) if info > WEIXIN_REDPACK_RESTRICTION_VALUE
+            money = Redpack.find(object.id).weixin_post(current_user,params[:beacon_url],info) if info > WEIXIN_REDPACK_RESTRICTION_VALUE
             return {'result' => 0, 'money' => money, 'info' => "成功发送种子红包"}
           else 
             return {'result' => -1, 'money' => 0, 'info' => "没有报名或者今天次数已经到限制"  }
