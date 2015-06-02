d9d2ee2145c2910ea32b135d6272733587c7313b
gilbertz <zhaoguoqi@sjtu.edu.cn>
2015-06-03 00:15:24 +0800

    cool

--- app/controllers/weitest_controller.rb
+++ app/controllers/weitest_controller.rb
@@ -198,12 +198,17 @@ class WeitestController < ApplicationController
   def game_report
     if current_user
       Record.create(:user_id => current_user.id, :from_user_id => params[:from_user_id], :beaconid=>@beacon.id, :game_id => params[:game_id], :sn=>params[:sn], :score => params[:score], :remark=>params[:remark])
-      rs = Record.where(:from_user_id => params[:from_user_id]).where('score > 9500').group('user_id')
-      if rs.length >= 4
+      if params[:score].to_i >= 9500 and current_user.social_value(@beacon.id) == 0
+        current_user.incr_social(@beacon.id)
+      end
+      rs = Record.where(:from_user_id => params[:from_user_id], :game_id=>params[:game_id], :feedback =>nil).where('score >= 9500').group('user_id')
+      if params[:score].to_i >= 9500 and rs.length >= 4
         f_value = 100 +rand(100)
         from_user = User.find( params[:from_user_id] )   
         @rp = @object.weixin_post(from_user, params[:beaconid], f_value) 
-        Record.create(:user_id => from_user.id, :beaconid=>@beacon.id, :game_id => params[:game_id], :score => rp, :object_type=>'g_redpack', :object_id => rp.id) 
+        Record.create(:user_id => from_user.id, :beaconid => @beacon.id, :game_id => params[:game_id], :score => @rp, :object_type => 'g_redpack',:object_id => @object.id)       
+        from_user.decr_social(@beacon.id) 
+        from_user.update_records(@beacon.id)
       end
     end
     render nothing: true
