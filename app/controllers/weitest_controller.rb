f802a00936f61f66a80b7c40e43b31e3f815316a
root <root@AY140521225939574bc7Z.(none)>
2015-06-05 00:47:24 +0800

    comment wizarcan sign

--- app/controllers/weitest_controller.rb
+++ app/controllers/weitest_controller.rb
@@ -2,7 +2,7 @@
 class WeitestController < ApplicationController
   before_filter :weixin_authorize, :only => [:o2o]
   before_filter :pre
-  before_filter :wizarcan_sign
+  #before_filter :wizarcan_sign
   skip_before_filter :verify_authenticity_token  
   
   def wizarcan_sign
