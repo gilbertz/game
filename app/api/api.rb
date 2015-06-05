12fa04a9083e466fa174559b0eef15ef902d3a77
gilbertz <gilbertzhaoguoqi@gmail.com>
2015-06-05 19:29:59 +0800

    red

--- app/api/api.rb
+++ app/api/api.rb
@@ -21,9 +21,11 @@ module API
       def current_user
         unless request.user_agent.downcase.index("micromessenger")
           #User.find_by_id(164)
+          p "pc"
           User.current_user
         else
           User.current_user
+          p "phone"
         end
       end
 
