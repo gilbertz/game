b54bf9d5b03110b2913f4e8452574b25d2602a61
gilbertz <zhaoguoqi@sjtu.edu.cn>
2015-06-09 15:25:33 +0800

    user ibeacon

--- app/controllers/weitest_controller.rb
+++ app/controllers/weitest_controller.rb
@@ -221,13 +221,15 @@ class WeitestController < ApplicationController
       rs = Record.where(:from_user_id => params[:from_user_id], :game_id=>params[:game_id], :feedback =>nil).where("score >= #{t_score}").group('user_id')
       rps = Record.where( :game_id=>params[:game_id], :beaconid => @beacon.id, :object_type => 'g_redpack')
       if params[:score].to_i >= t_score and rs.length >= t_num and from_user and from_user.social_value(@beacon.id) > 0
-        f_value = 100 +rand(20)
-        @rp = @object.weixin_post(from_user.id, @beacon.id, f_value)
-        #if @rp.to_i >０ 
+        #f_value = 100 +rand(20)
+        #@rp = @object.weixin_post(from_user.id, @beacon.id, f_value)
+        f_value = rand(20)
+        @rp = Pary.qy_pay(from_user.id, @beacon.get_merchant, f_value)
+        if @rp.to_i >０ 
           Record.create(:user_id => from_user.id, :beaconid => @beacon.id, :game_id => params[:game_id], :score => @rp, :object_type => 'g_redpack',:object_id => @object.id)       
           from_user.decr_social(@beacon.id) 
           from_user.update_records(@beacon.id)
-        #end
+        end
       end
     end
     render nothing: true
