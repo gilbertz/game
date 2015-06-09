030850c95a56d95e3b23aff747221f12aad30aba
gilbertz <zhaoguoqi@sjtu.edu.cn>
2015-06-09 09:00:28 +0800

    11

--- app/controllers/weitest_controller.rb
+++ app/controllers/weitest_controller.rb
@@ -223,11 +223,11 @@ class WeitestController < ApplicationController
       if params[:score].to_i >= t_score and rs.length >= t_num and from_user and from_user.social_value(@beacon.id) > 0
         f_value = 100 +rand(20)
         @rp = @object.weixin_post(from_user.id, @beacon.id, f_value)
-        if @rp.to_i >０ 
+        #if @rp.to_i >０ 
           Record.create(:user_id => from_user.id, :beaconid => @beacon.id, :game_id => params[:game_id], :score => @rp, :object_type => 'g_redpack',:object_id => @object.id)       
           from_user.decr_social(@beacon.id) 
           from_user.update_records(@beacon.id)
-        end
+        #end
       end
     end
     render nothing: true
