799ea3c016443972f9e793164fbcd6db0dd645af
root <root@AY140521225939574bc7Z.(none)>
2015-06-09 08:08:01 +0800

    update game report

--- app/controllers/weitest_controller.rb
+++ app/controllers/weitest_controller.rb
@@ -210,7 +210,7 @@ class WeitestController < ApplicationController
  
   def game_report
     t_score = 9500
-    t_num = 4
+    t_num = 6
     if current_user and current_user.social_value(@beacon.id) == 0 
       Record.create(:user_id => current_user.id, :from_user_id => params[:from_user_id], :beaconid=>@beacon.id, :game_id => params[:game_id], :sn=>params[:sn], :score => params[:score], :remark=>params[:remark])
      from_user = User.find_by_id( params[:from_user_id] ) 
@@ -221,11 +221,13 @@ class WeitestController < ApplicationController
       rs = Record.where(:from_user_id => params[:from_user_id], :game_id=>params[:game_id], :feedback =>nil).where("score >= #{t_score}").group('user_id')
       rps = Record.where( :game_id=>params[:game_id], :beaconid => @beacon.id, :object_type => 'g_redpack')
       if params[:score].to_i >= t_score and rs.length >= t_num and from_user
-        f_value = 100 +rand(50)
-        @rp = @object.weixin_post(from_user.id, @beacon.id, f_value) 
-        Record.create(:user_id => from_user.id, :beaconid => @beacon.id, :game_id => params[:game_id], :score => @rp, :object_type => 'g_redpack',:object_id => @object.id)       
-        from_user.decr_social(@beacon.id) 
-        from_user.update_records(@beacon.id)
+        f_value = 100 +rand(20)
+        @rp = @object.weixin_post(from_user.id, @beacon.id, f_value)
+        if @rp.to_i >０ 
+          Record.create(:user_id => from_user.id, :beaconid => @beacon.id, :game_id => params[:game_id], :score => @rp, :object_type => 'g_redpack',:object_id => @object.id)       
+          from_user.decr_social(@beacon.id) 
+          from_user.update_records(@beacon.id)
+        end
       end
     end
     render nothing: true
