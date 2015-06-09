ce98ff1f5af1c6ebbe0a86911296ef6dda8dc7e4
ct <ctaodream@163.com>
2015-06-09 17:11:41 +0800

    send pack

--- app/controllers/weitest_controller.rb
+++ app/controllers/weitest_controller.rb
@@ -221,15 +221,16 @@ class WeitestController < ApplicationController
       rs = Record.where(:from_user_id => params[:from_user_id], :game_id=>params[:game_id], :feedback =>nil).where("score >= #{t_score}").group('user_id')
       if params[:score].to_i >= t_score and rs.length >= t_num and from_user and from_user.social_value(@beacon.id) > 0
         #f_value = 100 +rand(20)
-        #@rp = @object.weixin_post(from_user.id, @beacon.id, f_value)
+        # @rp = @object.weixin_post(from_user.id, @beacon.id, f_value)
         f_value = 1+rand(10)
-        desc = '4个好友帮你抢到红包了，快去感谢他们哦'
-        @rp = Party.qy_pay(from_user.id, @beacon.get_merchant, f_value, desc)
-        #if @rp.to_i >０ 
+        # desc = '4个好友帮你抢到红包了，快去感谢他们哦'
+        # @rp = @object.qy_pay(from_user.id, @beacon.get_merchant, f_value, desc)
+        @rp = @object.send_pay(from_user.id,@beacon.id, 1,f_value)
+        if @rp.to_i > 0
           Record.create(:user_id => from_user.id, :beaconid => @beacon.id, :game_id => params[:game_id], :score => f_value, :object_type => 'g_redpack',:object_id => @object.id)       
           from_user.decr_social(@beacon.id) 
           from_user.update_records(@beacon.id)
-        #end
+        end
       end
     end
     render nothing: true
