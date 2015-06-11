8e1e25ece2c2c8956320453873650f3cb75219dd
gilbertz <gilbertzhaoguoqi@gmail.com>
2015-06-12 02:46:24 +0800

    red

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,9 @@ module Game
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths += Dir["#{config.root}/vendor/**/"]
+    config.assets.paths << Rails.root.join("app", "assets")
     config.assets.precompile += ["main.js", "main.css"]
+
 
     config.middleware.use(Rack::Config) do |env|
 
