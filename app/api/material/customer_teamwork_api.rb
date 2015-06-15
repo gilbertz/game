32c74a25a3098e98fe82095bfc75c9ae5c7c1cc7
ct <ctaodream@163.com>
2015-06-15 11:27:08 +0800

    teamwork first

--- app/api/material/customer_teamwork_api.rb
+++ app/api/material/customer_teamwork_api.rb
@@ -4,33 +4,86 @@ module CUSTOMER
       version 'v1'
       #------------------helpers------------------
       helpers do
+        #获取用户目前参与的团队任务
+        def self_in_teamwork(user_id,matterial_id)
+          teamwork_id = teamwork_key(user_id,matterial_id)
+          if teamwork_id
+            t = Teamwork.find_by_id(teamwork_id)
+            if t.status == 0
+              t
+            end
+          end
+        end
+
+
+        def teamwork_key(use_id,matterial_id)
+          "teamwork_#{matterial_id}_#{use_id}"
+        end
+
+
+        #用于同时加入某个团队协作排重
+        def teamwork_lock_key(teamwork_id,matterial_id)
+          "teamwork_lock_#{matterial_id}_#{teamwork_id}"
+        end
+
       end
 
       #------------------resources material------------------
       resources :teamwork do
         #--------------------post :start do-----------------
-        desc '开启一个团队合作模式'
+        desc '开启或加入一个团队合作模式'
         params do
+          optional :from_user,type: String,allow_blank: false,desc: '团队接龙发起者'
         end
-        post :start ,jbuilder:'material/teamwork_start' do
+        get :start ,jbuilder:'material/teamwork_start' do
+          @flag = -1
           if current_material
             @material = current_material
             @category = @material.category
             # 团队协作类型的模版
             if @category.game_type_id = 17
-              # 如果存在团队协作未完成
-              @exist_teamwork = Teamwork.where(:sponsor =>current_user.id,:material_id =>@material.id,:state => 0).first
-               unless @exist_teamwork
-                 init_percents = @material.teamworks_rand
-                 total_work = @material.total_work
-                 @teamwork = Teamwork.create_teamwork(current_user.id, @material.id, init_percents, total_work)
+              @exist_teamwork = self_in_teamwork(current_user.id,@material.id)
+              if @exist_teamwork
+                @flag = 0
+              else
+                #如果是加入到别人创建好的团队中
+                from_user = User.by_openid(params["from_user"])
+                if from_user && from_user.id != current_user.id
+                  @flag = 1
+                  @exist_teamwork = self_in_teamwork(from_user.id,@material.id)
+                  # 访问排重
+                  loop do
+                    if $redis.get(teamwork_lock_key(@exist_teamwork.id,@material.id)) != '1'
+                      $redis.set(teamwork_lock_key(@exist_teamwork.id,@material.id),'1')
+                      break
+                    end
+                  end
+                  if @exist_teamwork && @material.team_persons && @exist_teamwork.partners.count < @material.team_persons
+
+                    @exist_teamwork.add_partner(current_user.id)
+                    @exist_teamwork.save
+                  # teamwork 已经不存在了
+                  else
+                    @flag = 3
+                  end
+                  $redis.expire(teamwork_lock_key(@exist_teamwork.id,@material.id))
+               #自己创建一个
+                else
+                  @flag = 2
+                  init_percents = @material.teamworks_rand
+                  total_work = @material.total_work
+                  @teamwork = Teamwork.create_teamwork(current_user.id, @material.id, init_percents, total_work)
+                  if @teamwork
+                    $redis.set(teamwork_key(current_user.id, @material.id),@teamwork.id)
+                  end
                 end
-            end
-
+              end
+            end#if @category.game_type_id = 17
           end
-
         end
         #--------------------get :start do-----------------
+
+
 
         #--------------------post :join do---------------
         desc '加入一个团队合作模式'
@@ -60,6 +113,10 @@ module CUSTOMER
           end
         end
         #--------------------post :join do---------------
+
+
+        #---------------------post :结果
+
 
 
 
--- app/api/views/material/teamwork_start.jbuilder
+++ app/api/views/material/teamwork_start.jbuilder
@@ -1,13 +1,31 @@
-if @teamwork.present?
-  json.result 0
-  json.teamwork do
-    json.id = @teamwork.id
-    json.total_work = @teamwork.total_work
-    json.percent = @teamwork.get_user_percent(current_user.id)
-  end
-  json.user current_user
-  json.reward @material.team_reward
+# if @teamwork.present?
+#   json.result 0
+#   json.teamwork do
+#     json.id = @teamwork.id
+#     json.total_work = @teamwork.total_work
+#     json.percent = @teamwork.get_user_percent(current_user.id)
+#   end
+#   json.user current_user
+#   json.reward @material.team_reward
+#
+# else
+#
+# end
 
+json.result @flag
+#已经在某个团队合作中，且还未结束
+if @flag == 0
+
+#加入别人创建好的团队协作活动
+elsif @flag == 1
+
+#自己创建一个
+elsif @flag == 2
+
+# 手速慢了  teamwork 已经不存在了
+elsif @flag == 3
+
+# 没有这个应用 或者当前应用不是团队合作应用
 else
   render_api_error! '资源不存在'
 end
